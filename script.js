2df17a4198034df4ccb79dc0bb2d155a5fad3f3c
Flávio Santos <flavioraph@live.com>
2021-08-04 19:23:20 -0300

    atualizacao de eventos e manipulacao

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
-for (let i = 1; i <= 3; i++) {
-    let sectionTower = document.getElementById('torres')
-    let tower = document.createElement('div')
-    let vareta = document.createElement('div')
-    vareta.className = 'vareta' + `${i}`;
-    tower.className = 'torre' + `${i}`;
-    sectionTower.appendChild(tower)
-    tower.appendChild(vareta)
-    if (i === 1) {
-        geraDisco(vareta)
-    }
+const torre = document.querySelectorAll('.torre')
+const sectionTower = document.getElementById('torres')
+const torreInicial = document.getElementById('torre1')
+let takeDisc = undefined;
+let countClick = 0;
+
+let tamanho = 100
+for (let i = 1; i <= 4; i++) {
+    let disc = document.createElement('div')
+    disc.className = 'disc' + `${i}`
+    disc.id = 'disc'
+    disc.style.width = `${tamanho}` + 'px'
+    disc.style.height = '20px'
+    torreInicial.appendChild(disc)
+    tamanho -= 10;
 }
 
-function geraDisco(vareta) {
-    let tamanho = 40
-    let discos = [];
-    for (let i = 1; i <= 4; i++) {
-        let disc = document.createElement('div')
-        disc.className = 'disc' + `${i}`
-        disc.id = 'disc'
-        disc.style.width = `${tamanho}` + 'px'
-        discos.push({ disc: tamanho })
-        vareta.appendChild(disc)
-        tamanho += 20;
+sectionTower.addEventListener('click', function(e) {
+    if (e.target.className === 'torre1') {
+
     }
-}
+})
+for (let i = 0; i < torre.length; i++) {
+    torre[i].addEventListener('click', function(e) {
+        let actualTower = e.target
+        console.log(actualTower)
+        if (e.target.lastElementChild !== null) {
+            countClick = 1
+        }
+        if (countClick === 0) {
+            alert('Torre vazia, clique em outra')
+        } else {
+            if (e.target.lastElementChild === null || ) {
+
+            }
+        }
+    })
+
+    //criar variavel para armazenar disco
+    //primeiro click não pode ser em uma torre vazia 
+    //armazenar a torre clicada
+    //verificar qual é a proxima 
+    //se clicar em uma torre que não possui disco retornar jogada inválida 
+    //
