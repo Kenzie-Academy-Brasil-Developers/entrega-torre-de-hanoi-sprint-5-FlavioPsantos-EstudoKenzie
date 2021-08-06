b945addb988390adafc571360a272a103590d1a3
Flávio Santos <flavioraph@live.com>
2021-08-06 16:14:39 -0300

    Torre Funcional com pequenos Bugs

--- script.js
+++ script.js
@@ -4,46 +4,59 @@ const torreInicial = document.getElementById('torre1')
 let takeDisc = undefined;
 let countClick = 0;
 let actualClick;
+let tamanhoDiscoAtual = 0
 
 let tamanho = 100
 for (let i = 1; i <= 4; i++) {
     let disc = document.createElement('div')
-    disc.className = 'disc' + `${i}`
-    disc.id = 'disc'
+    disc.className = 'disc'
+    disc.id = 'disc' + `${i}`
     disc.style.width = `${tamanho}` + 'px'
     disc.style.height = '20px'
     torreInicial.appendChild(disc)
     tamanho -= 10;
 }
-
 sectionTower.addEventListener('click', function(e) {
-    if (e.target.className === 'torre1') {
+    let actualTower = e.target
+    if (actualTower.id === 'torres') {
 
     }
-})
-for (let i = 0; i < torre.length; i++) {
-    torre[i].addEventListener('click', function(e) {
-        const actualTower = e.target
-        actualClick = actualTower
-        console.log(actualTower)
-        if (e.target.lastElementChild !== null) {
-            countClick = 1
-        }
-        if (countClick === 0) {
-            alert('Torre vazia, clique em outra')
-        } else {
-            if (countClick === 1) {
-                actualTower = e.target
-                actualClick.appendChild(takeDisc)
+    console.log(actualTower)
+    actualClick = document.getElementById(e.target.id)
+    console.log(actualClick)
+
+    console.log(e.target)
+    if (actualTower.lastElementChild !== null && countClick === 0) {
+        takeDisc = document.getElementById(actualTower.lastElementChild.id)
+        tamanhoDiscoAtual = takeDisc.clientWidth
+            // console.log(tamanhoDiscoAtual)
+            // console.log(actualTower.lastElementChild)
+        actualTower.removeChild(actualTower.lastElementChild)
+        countClick = 1
+
+    } else {
+        if (countClick !== 0 && takeDisc !== undefined && actualTower.className === 'torre') {
+            if (actualTower.lastElementChild === null || tamanhoDiscoAtual < actualTower.lastElementChild.clientWidth) {
+
+                actualTower.appendChild(takeDisc)
+                countClick = 0
+
+            } else {
+                alert('JOGADA INVÁLIDA')
+            }
+            if (actualTower.id !== torreInicial.id && actualTower.className === 'torre') {
+                if (actualTower.childElementCount === 4) {
+                    alert('VOCÊ GANHOU')
+                }
             }
         }
+        if (actualTower.lastElementChild === null) {
+            alert('Torre inválida')
+        }
 
-    })
-}
+    }
 
-//criar variavel para armazenar disco
-//primeiro click não pode ser em uma torre vazia 
-//armazenar a torre clicada
-//verificar qual é a proxima 
-//se clicar em uma torre que não possui disco retornar jogada inválida 
-//
+
+
+
+})
